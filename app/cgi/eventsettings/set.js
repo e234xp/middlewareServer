2ab5fb187c419151767202cd73eae7b3928f6421
Gavin <gavin@inncom.cloud>
2023-06-19 16:22:47 +0800

    setting 特定欄位確保有 uuid

--- app/cgi/eventsettings/set.js
+++ app/cgi/eventsettings/set.js
@@ -1,4 +1,33 @@
+const { uuid: uuidv4 } = require('uuidv4');
+
 module.exports = (data) => {
+  if (data.line_actions_list) {
+    data.line_actions_list = data.line_actions_list.map((item) => {
+      if (!item.uuid) {
+        item.uuid = uuidv4();
+      }
+      return item;
+    });
+  }
+
+  if (data.mail_actions_list) {
+    data.mail_actions_list = data.mail_actions_list.map((item) => {
+      if (!item.uuid) {
+        item.uuid = uuidv4();
+      }
+      return item;
+    });
+  }
+
+  if (data.http_actions_list) {
+    data.http_actions_list = data.http_actions_list.map((item) => {
+      if (!item.uuid) {
+        item.uuid = uuidv4();
+      }
+      return item;
+    });
+  }
+
   global.spiderman.db.eventsettings.updateOne({}, data);
 
   return {
--- app/cgi/tabletsettings/set.js
+++ app/cgi/tabletsettings/set.js
@@ -1,6 +1,26 @@
-module.exports = (data) => {
-  global.spiderman.db.settings.updateOne({}, data);
+const { uuid: uuidv4 } = require('uuidv4');
 
+module.exports = (data) => {
+  if (data.access_control_schedule_list) {
+    data.access_control_schedule_list = data.access_control_schedule_list.map((item) => {
+      if (!item.uuid) {
+        item.uuid = uuidv4();
+      }
+      return item;
+    });
+  }
+
+  if (data.high_temperature_sound_alert_schedule_list) {
+    data.high_temperature_sound_alert_schedule_list = data
+      .high_temperature_sound_alert_schedule_list.map((item) => {
+        if (!item.uuid) {
+          item.uuid = uuidv4();
+        }
+        return item;
+      });
+  }
+
+  global.spiderman.db.settings.updateOne({}, data);
   return {
     message: 'ok',
   };
