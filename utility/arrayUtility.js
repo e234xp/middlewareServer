f4602e5b04ce69dee8e49d899013f6154843df82
Gavin <gavin@inncom.cloud>
2023-06-28 12:46:56 +0800

    刪除舊架構

--- utility/arrayUtility.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-exports.removeItemFromArray = function( array, key, value ) {
-    let removeIndex = array.map( function(item){ return eval( 'item.' + key ); }).indexOf( value );
-    if( removeIndex > -1 ) {
-        array.splice(removeIndex, 1);
-    }
-}
